9d7940c43be006eb5be015c00c73fcc9e04e3f71
yommexg <hydrokidb@gmail.com>
2025-01-11 12:18:21 +0000

    Bottom Tab setup

---
+++ src/screens/navigation/TabNavigation/HomeNavigation.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const HomeNavigation = () => {
+  return (
+    <View>
+      <Text>HomeNavigation</Text>
+    </View>
+  );
+};
+
+export default HomeNavigation;
